db62e4a4539eaf701a1cd6cf04f626cd1b17a3c2
Suad <suadkamardeen@gmail.com>
2019-10-11 19:14:38 +0200

    Configure scoring systems.

--- coder_invasion.py
+++ coder_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,8 +26,11 @@ class CoderInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Coder Invasion")
         
-        # Create an instance to store game statistics.
+        
+        # Create an instance to store game statistics,
+        #   and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -108,9 +112,14 @@ class CoderInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
             
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -175,10 +184,21 @@ class CoderInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
         
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+        
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+            
+            # Increase level.
+            self.stats.level +=1
+            self.sb.prep_level()
             
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
@@ -223,6 +243,9 @@ class CoderInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        
+        # Draw the score information.
+        self.sb.show_score()
         
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,10 @@ class GameStats:
         # Start game in an inactive state.
         self.game_active = False
         
+        # High score should never be reset.
+        self.high_score = 0 
+        
     def reset_stats(self):
         """Initialise statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
